15600a28034e380f73f264bce38c373f84f94e41
Akshansh <akshanshdubey.dev@gmail.com>
2026-01-17 20:52:42 +0530

    ..frontend

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const app = express();
+
 app.use(express.json());
 app.use(cors());
 app.use(express.static("../frontend/dist"))
 
 app.get("*", () => {
-    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"))
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
 }
 )
 app.listen(5000, () => {
