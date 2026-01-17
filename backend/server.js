abcca1e3761ad52856bf1e43592a8b679e134203
Akshansh <akshanshdubey.dev@gmail.com>
2026-01-17 20:43:53 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const cors = require('cors');
-
+const path = require('path');
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(express.static("../frontend/dist"))
 
+app.get("*", () => {
+    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"))
+}
+)
 app.listen(5000, () => {
     console.log('Server is running on port 5000');
 });
-
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,25 @@
-import React from 'react'
-import axios from 'axios'
-import { useState } from 'react';
-
+import React from "react";
+import axios from "axios";
+import { useState } from "react";
 
 const App = () => {
-const [result,setResult]=useState("");
+  const [result, setResult] = useState("");
+
+  //akshanshdemo-fkd8f7ffa2eae4bw.southindia-01.azurewebsites.net
 
   const fetchData = async () => {
     try {
-      const response = await axios.get('http://localhost:5000');
+      const response = await axios.get(
+        "http://akshanshdemo-fkd8f7ffa2eae4bw.southindia-01.azurewebsites.net",
+      );
       setResult(response.data);
       console.log(response);
     } catch (error) {
       console.error(error);
     }
-  }
+  };
   fetchData();
-  return (
-    <div>{result}</div>
-  )
-}
+  return <div>{result}</div>;
+};
 
-export default App
+export default App;
