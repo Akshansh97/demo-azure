f05bf230e07c67607fcffc9871ad5a0fb71d43ef
Akshansh <akshanshdubey.dev@gmail.com>
2026-01-17 21:06:20 +0530

    dist2

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,17 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const app = express();
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
-app.use(express.static("../frontend/dist"))
 
-app.use(
-  express.static(path.join(__dirname, "../frontend/dist/index.html"))
-);
-app.listen(5000, () => {
+app.use(express.static(path.join(__dirname, "../frontend/dist")))
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
+})
+
+app.listen(port, () => {
     console.log('Server is running on port 5000');
 });
 app.get('/', (req, res) => {
