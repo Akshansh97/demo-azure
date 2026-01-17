921b66ca3a8fae1a9141e69bee76fcfc45bae709
Akshansh <akshanshdubey.dev@gmail.com>
2026-01-17 22:10:58 +0530

    port

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
 })
 
-app.listen(port, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log('Server is running on port 5000');
 });
 app.get('/', (req, res) => {
